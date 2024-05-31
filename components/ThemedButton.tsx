6c9be548da887999656859a7797adcffdb03ccd5
Wouter de Bruijn <wouter@hedium.nl>
2024-05-31 19:33:03 +0200

    ✨ Added disabled property

--- components/ThemedButton.tsx
+++ components/ThemedButton.tsx
@@ -1,3 +1,4 @@
+import { Colors } from "@/constants/Colors";
 import { useThemeColor } from "@/hooks/useThemeColor";
 import { ActivityIndicator, Pressable, StyleSheet, Text } from "react-native";
 
@@ -5,15 +6,20 @@ interface ThemedButtonProps {
   title: string;
   onPress: () => void;
   loading?: boolean;
+  disabled?: boolean;
 }
 
-export default function ThemedButton({ title, onPress, loading = false }: ThemedButtonProps) {
+export default function ThemedButton({ title, onPress, loading = false, disabled = false }: ThemedButtonProps) {
 
   const tint = useThemeColor({}, 'tint');
-  const text = useThemeColor({}, 'text');
+  const text = Colors.dark.text;
 
   return (
-    <Pressable onPress={onPress} style={[styles.button, { backgroundColor: tint }]}>
+    <Pressable
+      onPress={onPress}
+      style={[styles.button, { backgroundColor: tint }, disabled ? styles.button_disabled : null]}
+      disabled={disabled}
+    >
       <ActivityIndicator animating={loading} style={[styles.activityIndicator]} />
       <Text style={[{ color: text }]}>{loading ? 'loading' : title}</Text>
     </Pressable>
@@ -26,10 +32,14 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 4,
     alignItems: 'center',
+    elevation: 1
+  },
+  button_disabled: {
+    opacity: 0.4
   },
   activityIndicator: {
     position: 'absolute',
     right: 0,
     bottom: 8,
-  }
+  },
 })
