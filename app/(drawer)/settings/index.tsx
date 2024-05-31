493d3322119e1ddd0f3f3721431adf250fc4bd5a
Wouter de Bruijn <wouter@hedium.nl>
2024-05-31 19:11:54 +0200

    🔥 Removed temporary skeleton

--- app/(drawer)/settings/index.tsx
+++ app/(drawer)/settings/index.tsx
@@ -5,7 +5,6 @@ import { ThemedText } from "@/components/ThemedText";
 import CameraSettings from "@/components/settings/CameraSettings";
 
 import ServerSettings from "@/components/settings/ServerSettings";
-import SkeletonView from "@/components/skeletons/SkeletonView";
 import { Suspense } from "react";
 
 
@@ -19,7 +18,7 @@ export default function Settings() {
       } headerBackgroundColor={{ dark: '#333', light: '#f4f4f4' }}>
       <ThemedText type="subtitle">Frigate Server:</ThemedText>
 
-      <Suspense fallback={<SkeletonView height={200} width={400} />}>
+      <Suspense>
         <ServerSettings />
       </Suspense>
 
